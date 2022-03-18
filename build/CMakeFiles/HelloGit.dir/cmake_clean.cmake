36c1a2450486262b4f272edecacd202349309890
贤 邵 <S760824822@outlook.com>
2022-03-18 12:16:35 +0800

    2022.3.18 12:16

---
+++ build/CMakeFiles/HelloGit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloGit.dir/main.cpp.obj"
+  "CMakeFiles/HelloGit.dir/main.cpp.obj.d"
+  "HelloGit.exe"
+  "HelloGit.exe.manifest"
+  "HelloGit.pdb"
+  "libHelloGit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
